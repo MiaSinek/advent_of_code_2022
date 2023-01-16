92ba32ca7e16931376a4a5a36134f9c3d07edef3
Mia Sinek <mia@miasinek.com>
2023-01-16 20:44:12 +0100

    WIP

--- day_10_cathode_ray_tube/day_10a.rb
+++ day_10_cathode_ray_tube/day_10a.rb
@@ -19,13 +19,14 @@ class CathodeRayTube
         p "signal_strength: #{signal_strength}, x_register: #{x_register}, cycle: #{cycle}"
       end
 
-      # we need the x_register value in the cycle and not at the end of the cycle -> increasing the cycle num here
+      p "x_register: #{x_register} jsut pumping up the cycle, no effect on x_register, cycle: #{cycle}"
       cycle += 1
 
       case action
       when "noop"
         # no effect on x_register, takes 1 cycle to complete
       when "addx"
+        p "addx: #{value}, cycle: #{cycle} , x_register: #{x_register}"
         # adds value to x_register, takes 2 cycle to complete
         if cycle % 40 == 20
           signal_strength += x_register * cycle
